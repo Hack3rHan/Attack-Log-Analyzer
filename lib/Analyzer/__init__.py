c79a9deafce502e183dab02e53da5d5d2cd317af
Hack3rHan <wangxh_cn@qq.com>
2020-06-07 10:20:02 +0800

    删除文件 lib/Analyzer

--- lib/Analyzer/__init__.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python
-# -*- coding: UTF-8 -*-
-
-from lib.analyzer.analyzer import Analyzer
-from lib.analyzer.apache_analyzer import ApacheAnalyzer
-from lib.analyzer.nginx_analyzer import NginxAnalyzer
-from lib.analyzer.iis_analyzer import IISAnalyzer
